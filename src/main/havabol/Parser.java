f3e7e716a16852cd33df396c4ea49115ff656841
fish <fish@Fish>
2017-03-02 13:45:37 -0600

    Updated the parser a bit. THe declaration statement now checks for a complete statement before moving on. I also added a placeholder for when we add in Int i = 2; functionality.

--- src/main/havabol/Parser.java
+++ src/main/havabol/Parser.java
@@ -39,56 +39,84 @@ public class Parser {
             return(true);
         }
     }
+    
+    
     void statement(Scanner scanner) throws IOException, errorCatch{
         
-        while(!scanner.currentToken.tokenStr.equals(";")){
-           
-                
-                //check what the current symbol is
-                switch(scanner.currentToken.primClassif){
-                    
-                    //Look for control tokens
-                    case Token.CONTROL:
+         //check what the current symbol is
+        switch(scanner.currentToken.primClassif){
+                   
+           //Look for control tokens
+           case Token.CONTROL:
                         
-                        //If the control token is a declare token, add it to the
-                        //Symbol table
-                        if(scanner.currentToken.subClassif == Token.DECLARE){
-                           scanner.getNext();
-                           System.out.println(scanner.currentToken.tokenStr + " <<<<");
-                           declareStatement(scanner.currentToken);
-                           //IF the next token is a ; just retun now, nothing else to do
-                           if(scanner.nextToken.tokenStr.equals(";")){
-                               return;
-                           }
-                        }
-                        break;
-                    default:
+           //If the control token is a declare token, add it to the
+           //Symbol table
+           switch(scanner.currentToken.subClassif){
+               
+               //if I dentifier run declare staement code
+               case Token.DECLARE:
+                   
+                   declareStatement(scanner);
+           }
+                           
+           default:
                        // System.out.println(scanner.currentToken.tokenStr + "   Got here");
                     
                 }
             
-            scanner.getNext();
-        }
+            
+        
         
     }
     
-    
-    void declareStatement(Token token){
+    //Assumes that the current token is a control declaration ie. Int, Float...
+    void declareStatement(Scanner scan) throws IOException, errorCatch{
         
-        //Chekc to make sure correct type of token was passed in
-        if(token.primClassif != Token.OPERAND && token.subClassif != Token.IDENTIFIER){
-            System.out.println("Error expected identifier");
+        scan.getNext();
+                
+        if(scan.currentToken.primClassif != Token.OPERAND && scan.currentToken.subClassif != Token.IDENTIFIER){
+            System.out.println("Error expected and Identifier");
             System.exit(-1);
-        }
-        
-        //make sure symbol table entry doesn not already exist
-        if(!checkSymbol(token)){
-            sbTable.putSymbol(token, new STEntry(token.tokenStr, Primary.OPERAND, Subclass.IDENTIFIER));
         }else{
-            System.out.println("Error double declaration");
-            System.exit(-1);
+            if(!checkSymbol(scan.currentToken)){
+                sbTable.putSymbol(scan.currentToken, 
+                new STEntry(scan.currentToken.tokenStr, Primary.OPERAND, Subclass.IDENTIFIER));
+            }else{
+                System.out.println("Error double declaration");
+                System.exit(-1);
+            }
+                   
         }
-
         
+        //get the next token to check for correct items after
+        scan.getNext();
+        
+        switch(scan.currentToken.primClassif){
+            
+            //Makes sure it is folloed by a ;
+            case Token.SEPARATOR:
+                if(!scan.currentToken.tokenStr.equals(";")){
+                   System.out.println("Error Expected a ;");
+                   System.exit(-1);
+                }else{
+                    return;
+                }
+                
+            //Doesn't do much yet added this for future use
+            //This will check for an = sign for declaration and
+            //setting a value to it at the same time
+            case Token.OPERATOR:
+                if(!scan.currentToken.tokenStr.equals("=")){
+                   System.out.println("Error Expected a =");
+                   System.exit(-1);
+                }else{
+                    //Code for proceccing statments here
+                }
+                
+            default:
+                System.out.println("Error Expected a ; or =");
+                System.exit(-1);
+        }
+            
     }
 }
