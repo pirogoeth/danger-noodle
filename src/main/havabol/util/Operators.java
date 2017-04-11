98f35a66a86a2fd2cff91ea9c22fd87184cba185
fish <fish@Fish>
2017-04-11 17:50:18 -0500

    added a Operators class under util

---
+++ src/main/havabol/util/Operators.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.havabol.util;
+
+import havabol.common.type.TypeInterface;
+
+/**
+ *
+ * @author fish
+ */
+public class Operators {
+    
+    
+    
+    
+    
+    
+    public TypeInterface add(TypeInterface first, TypeInterface second){
+        
+        
+       
+        
+        
+        
+    }
+}
