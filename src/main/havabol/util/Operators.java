bb3da252e577f5cbb42b8488e94880892a5d2491
fish <fish@Fish>
2017-04-11 19:42:09 -0500

    added == != > and < <= and >= are next

--- src/main/havabol/util/Operators.java
+++ src/main/havabol/util/Operators.java
@@ -215,4 +215,285 @@ public class Operators {
         
         return(null);
     }
+    
+    public static TypeInterface equal(TypeInterface first, TypeInterface second) {
+        switch (first.getFormalType()) {
+            case FLOAT:
+                PFloat floatB;
+                PBoolean BoolRes = null;
+                switch (second.getFormalType()) {
+                    case INTEGER:
+                        floatB = floatPrim((PInteger) second);
+                        break;
+                    case FLOAT:
+                        floatB = (PFloat) second;
+                        break;
+                    case STRING:
+                        floatB = floatPrim((PString) second);
+                        break;
+                    default:
+                        // XXX - EXPLODE!
+                        return null;
+                }
+                if( ((PFloat) first).getValue() == floatB.getValue()){
+                    BoolRes.setValue(Boolean.TRUE);
+                }else{
+                    BoolRes.setValue(Boolean.FALSE);
+                }
+                return BoolRes;
+            case INTEGER:
+                PInteger intB;
+                PBoolean boolRes = null;
+                switch (second.getFormalType()) {
+                    case INTEGER:
+                        intB = (PInteger) second;
+                        break;
+                    case FLOAT:
+                        intB = intPrim((PFloat) second);
+                        break;
+                    case STRING:
+                        intB = intPrim((PString) second);
+                        break;
+                    default:
+                        // XXX - EXPLODE!
+                        return null;
+                }
+                
+               if( ((PInteger) first).getValue() == intB.getValue()){
+                   boolRes.setValue(Boolean.TRUE);
+               }else{
+                    boolRes.setValue(Boolean.FALSE);
+               }
+                return boolRes;
+            case BOOLEAN:
+                PBoolean boolReS = null;
+                if((PBoolean) first.getValue() == (PBoolean) second.getValue() ){
+                    boolReS.setValue(Boolean.TRUE);
+                }else{
+                    boolReS.setValue(Boolean.FALSE);
+                }
+                // XXX - We need a narrow exception class for this.
+                return boolReS;
+                
+            case STRING:
+                PBoolean bOolReS = null;
+                if(((PString) first).getValue().equals(((PString) second).getValue())){
+                        
+                bOolReS.setValue(Boolean.TRUE);
+                }else{
+                    bOolReS.setValue(Boolean.FALSE);
+                }
+                return bOolReS;
+            default:
+                return null;
+        }
+    }
+    
+    
+    public static TypeInterface notequal(TypeInterface first, TypeInterface second) {
+        switch (first.getFormalType()) {
+            case FLOAT:
+                PFloat floatB;
+                PBoolean BoolRes = null;
+                switch (second.getFormalType()) {
+                    case INTEGER:
+                        floatB = floatPrim((PInteger) second);
+                        break;
+                    case FLOAT:
+                        floatB = (PFloat) second;
+                        break;
+                    case STRING:
+                        floatB = floatPrim((PString) second);
+                        break;
+                    default:
+                        // XXX - EXPLODE!
+                        return null;
+                }
+                if( ((PFloat) first).getValue() != floatB.getValue()){
+                    BoolRes.setValue(Boolean.TRUE);
+                }else{
+                    BoolRes.setValue(Boolean.FALSE);
+                }
+                return BoolRes;
+            case INTEGER:
+                PInteger intB;
+                PBoolean boolRes = null;
+                switch (second.getFormalType()) {
+                    case INTEGER:
+                        intB = (PInteger) second;
+                        break;
+                    case FLOAT:
+                        intB = intPrim((PFloat) second);
+                        break;
+                    case STRING:
+                        intB = intPrim((PString) second);
+                        break;
+                    default:
+                        // XXX - EXPLODE!
+                        return null;
+                }
+                
+               if( ((PInteger) first).getValue() != intB.getValue()){
+                   boolRes.setValue(Boolean.TRUE);
+               }else{
+                    boolRes.setValue(Boolean.FALSE);
+               }
+                return boolRes;
+            case BOOLEAN:
+                PBoolean boolReS = null;
+                if((PBoolean) first.getValue() != (PBoolean) second.getValue() ){
+                    boolReS.setValue(Boolean.TRUE);
+                }else{
+                    boolReS.setValue(Boolean.FALSE);
+                }
+                // XXX - We need a narrow exception class for this.
+                return boolReS;
+                
+            case STRING:
+                PBoolean bOolReS = null;
+                if(!((PString) first).getValue().equals(((PString) second).getValue())){
+                        
+                bOolReS.setValue(Boolean.TRUE);
+                }else{
+                    bOolReS.setValue(Boolean.FALSE);
+                }
+                return bOolReS;
+            default:
+                return null;
+        }
+    }
+    
+    public static TypeInterface greaterthan(TypeInterface first, TypeInterface second) {
+        switch (first.getFormalType()) {
+            case FLOAT:
+                PFloat floatB;
+                PBoolean BoolRes = null;
+                switch (second.getFormalType()) {
+                    case INTEGER:
+                        floatB = floatPrim((PInteger) second);
+                        break;
+                    case FLOAT:
+                        floatB = (PFloat) second;
+                        break;
+                    case STRING:
+                        floatB = floatPrim((PString) second);
+                        break;
+                    default:
+                        // XXX - EXPLODE!
+                        return null;
+                }
+                if( ((PFloat) first).getValue() > floatB.getValue()){
+                    BoolRes.setValue(Boolean.TRUE);
+                }else{
+                    BoolRes.setValue(Boolean.FALSE);
+                }
+                return BoolRes;
+            case INTEGER:
+                PInteger intB;
+                PBoolean boolRes = null;
+                switch (second.getFormalType()) {
+                    case INTEGER:
+                        intB = (PInteger) second;
+                        break;
+                    case FLOAT:
+                        intB = intPrim((PFloat) second);
+                        break;
+                    case STRING:
+                        intB = intPrim((PString) second);
+                        break;
+                    default:
+                        // XXX - EXPLODE!
+                        return null;
+                }
+                
+               if( ((PInteger) first).getValue() > intB.getValue()){
+                   boolRes.setValue(Boolean.TRUE);
+               }else{
+                    boolRes.setValue(Boolean.FALSE);
+               }
+                return boolRes;
+            case BOOLEAN:
+                System.out.println("Error can not do greter than comparision of booleans");
+                return null;
+                
+            case STRING:
+                PBoolean bOolReS = null;
+                if(((PString) first).getValue().compareTo(((PString) second).getValue()) > 0){
+                        
+                bOolReS.setValue(Boolean.TRUE);
+                }else{
+                    bOolReS.setValue(Boolean.FALSE);
+                }
+                return bOolReS;
+            default:
+                return null;
+        }
+    }
+    
+    public static TypeInterface lassthan(TypeInterface first, TypeInterface second) {
+        switch (first.getFormalType()) {
+            case FLOAT:
+                PFloat floatB;
+                PBoolean BoolRes = null;
+                switch (second.getFormalType()) {
+                    case INTEGER:
+                        floatB = floatPrim((PInteger) second);
+                        break;
+                    case FLOAT:
+                        floatB = (PFloat) second;
+                        break;
+                    case STRING:
+                        floatB = floatPrim((PString) second);
+                        break;
+                    default:
+                        // XXX - EXPLODE!
+                        return null;
+                }
+                if( ((PFloat) first).getValue() < floatB.getValue()){
+                    BoolRes.setValue(Boolean.TRUE);
+                }else{
+                    BoolRes.setValue(Boolean.FALSE);
+                }
+                return BoolRes;
+            case INTEGER:
+                PInteger intB;
+                PBoolean boolRes = null;
+                switch (second.getFormalType()) {
+                    case INTEGER:
+                        intB = (PInteger) second;
+                        break;
+                    case FLOAT:
+                        intB = intPrim((PFloat) second);
+                        break;
+                    case STRING:
+                        intB = intPrim((PString) second);
+                        break;
+                    default:
+                        // XXX - EXPLODE!
+                        return null;
+                }
+                
+               if( ((PInteger) first).getValue() < intB.getValue()){
+                   boolRes.setValue(Boolean.TRUE);
+               }else{
+                    boolRes.setValue(Boolean.FALSE);
+               }
+                return boolRes;
+            case BOOLEAN:
+                System.out.println("Error can not do greter than comparision of booleans");
+                return null;
+                
+            case STRING:
+                PBoolean bOolReS = null;
+                if(((PString) first).getValue().compareTo(((PString) second).getValue()) < 0){
+                        
+                bOolReS.setValue(Boolean.TRUE);
+                }else{
+                    bOolReS.setValue(Boolean.FALSE);
+                }
+                return bOolReS;
+            default:
+                return null;
+        }
+    }
 }
